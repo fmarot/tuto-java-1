b456a80567523be668958c0be1f9a3a487a08beb
pierre <pi.marot@gmail.com>
2017-11-25 20:14:52 +0100

    main orienté objet

--- src/PasserAGue.java
+++ src/PasserAGue.java
@@ -1,17 +1,20 @@
 
-public class Passer_a_gue {
+public class PasserAGue {
 
 	private static final int VALEUR_CIBLE = 100;
 
-	public static void main(String[] args) {
-		int longueurPont = 0;
-		Joueur joueurEnCours = null;
+	private int longueurPont;
+	private Joueur[] joueurs;
 
+	public PasserAGue() {
+		longueurPont = 0;
 		Joueur joueur1 = new JoueurHumain("Vous");
 		Joueur joueur2 = new JoueurOrdi("Moi");
+		joueurs = new Joueur[] { joueur1, joueur2 };
+	}
 
-		Joueur[] joueurs = { joueur1, joueur2 };
-
+	public void jouer() {
+		Joueur joueurEnCours = null;
 		int indexTour = 0;
 		while (longueurPont < VALEUR_CIBLE) {
 			joueurEnCours = joueurs[indexTour % (joueurs.length)];
@@ -22,4 +25,8 @@ public class Passer_a_gue {
 		System.out.println("Bravo " + joueurEnCours.getNom());
 	}
 
+	public static void main(String[] args) {
+		PasserAGue passerAGue = new PasserAGue();
+		passerAGue.jouer();
+	}
 }
